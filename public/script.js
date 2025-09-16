32aaa0e653bc49700eb83a53249f3f52f20ccce3
Arbenita <141353698+arbenitacanaj4@users.noreply.github.com>
2025-09-16 17:44:09 +0200

    autocollapse

--- public/script.js
+++ public/script.js
@@ -41,6 +41,23 @@ function initSmoothScrolling() {
     })
   })
 }
+function initNavbarAutoCollapse() {
+  const navLinks = document.querySelectorAll('.navbar-collapse .nav-link');
+  const navbarCollapse = document.querySelector('.navbar-collapse');
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      if (navbarCollapse.classList.contains('show')) {
+        const bsCollapse = new bootstrap.Collapse(navbarCollapse, { toggle: true });
+        bsCollapse.hide();
+      }
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initNavbarAutoCollapse();
+});
 
 function initFormValidation() {
   const form = document.getElementById("regForm")
